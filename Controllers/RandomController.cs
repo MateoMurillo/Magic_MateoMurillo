6d67035b98dba6cb26559f77bb36b7a91fb46100
Mateo Murillo <a2020117584@estudiantes.upsa.edu.bo>
2021-10-13 21:17:21 -0400

    Controlador Random creado

---
+++ Controllers/RandomController.cs
@@ -0,0 +1,44 @@
+﻿
+using Magic_MateoMurillo.Data;
+using Magic_MateoMurillo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancionesWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RandomController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Suerte>> GetCancion()
+        {
+
+            var list = await _context.Suerte.ToListAsync();
+
+            var max = list.Count;
+            int index = new Random().Next(0, max);
+
+            var Suerte = list[index];
+
+            if (Suerte == null)
+            {
+                return NoContent();
+            }
+
+            return Suerte;
+        }
+    }
+}
