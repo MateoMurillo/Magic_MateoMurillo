fdaba330faed96201ad9499b026d06483c07322f
Mateo Murillo <a2020117584@estudiantes.upsa.edu.bo>
2021-10-13 20:54:20 -0400

    Modelo creado

--- Models/Suerte.cs
+++ Models/Suerte.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace Magic_MateoMurillo.Models
 {
     public class Suerte
     {
+        [Key]
+        public string FuturoId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        public string Vision { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        [Url]
+        public string Imagen { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
     }
 }
